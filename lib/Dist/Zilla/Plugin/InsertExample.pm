ff1db112c13fb0454f9f494e5f95caf1112e42ec
Graham Ollis <perl@wdlabs.com>
2013-06-17 06:26:39 -0400

    initial implementation

--- lib/Dist/Zilla/Plugin/InsertExample.pm
+++ lib/Dist/Zilla/Plugin/InsertExample.pm
@@ -3,8 +3,75 @@ package Dist::Zilla::Plugin::InsertExample;
 use strict;
 use warnings;
 use v5.10;
+use Moose;
 
 # ABSTRACT: Insert example into your POD from a file
 # VERSION
+
+=head1 SYNOPSIS
+
+In your dist.ini:
+
+ [InsertExample]
+
+In your POD:
+
+ =head1 EXAMPLE
+ 
+ Here is an exaple that writes hello world to the terminal:
+ 
+ # EXAMPLE: example/hello.pl
+
+File in your dist named example/hello.pl
+
+ #!/usr/bin/perl
+ say 'hello world';
+
+=head1 DESCRIPTION
+
+This plugin takes examples included in your distribution and
+inserts them in your POD where you have an EXAMPLE directive.
+This allows you to keep a version in the distribution which
+can be run by you and your users, as well as making it
+available in your POD documentation, without the need for 
+updating example scripts in multiple places.
+
+=cut
+
+with 'Dist::Zilla::Role::FileMunger';
+with 'Dist::Zilla::Role::FileFinderUser' => {
+  default_finders => [ qw( :InstallModules :ExecFiles ) ],
+};
+
+sub munge_files
+{
+  my($self) = @_;
+  $self->munge_file($_) for @{ $self->found_files };
+}
+
+sub munge_file
+{
+  my($self, $file) = @_;
+
+  my $content = $file->content;
+  if($content =~ s{^#\s*EXAMPLE:\s*(.*)\s*$}{$self->_slurp_example($1)."\n"}meg)
+  {
+    $self->log([ 'adding examples in %s', $file->name]);
+    $file->content($content);
+  }
+}
+
+sub _slurp_example
+{
+  my($self, $filename) = @_;
+  my $file = $self->zilla->root->file($filename);
+  unless(-r $file)
+  {
+    $self->log_fatal("no such example file $filename");
+  }
+  join "\n", map { " $_" } split /\n\r?/, $file->slurp;
+}
+
+__PACKAGE__->meta->make_immutable;
 
 1;
---
+++ t/basic.t
@@ -0,0 +1,25 @@
+use strict;
+use warnings;
+use Test::More 0.88;
+use Test::DZil;
+
+# basing this on the [ModuleBuildDatabase] test for now
+plan tests => 1;
+
+my $tzil = Builder->from_config(
+  { dist_root => 'corpus/DZT' },
+  { 
+    add_files => { 
+      'source/dist.ini' => simple_ini(
+        {},
+        'GatherDir',
+        [ 'InsertExample' => {} ],
+      )
+    }
+  }
+);
+
+$tzil->build;
+
+my($pm) = grep { $_->name eq 'lib/DZT.pm' } @{ $tzil->files };
+like $pm->content, qr{^ say 'hello world';$}m, "module contains example file";
